d7f4b5b12827dbf254902f85537f734a93e93ec3
Tyler Nichols <tylernichols@Tylers-MacBook-Pro-2.local>
2017-01-31 20:15:43 -1000

    put request works

--- routes/home.js
+++ routes/home.js
@@ -7,7 +7,6 @@ const bodyParser = require('body-parser');
 
 router.route('/')
   .get((req, res) => {
-    console.log('req: ', req);
     Card.findAll()
       .then(card => {
         res.json(card);
@@ -30,6 +29,40 @@ router.route('/')
     })
     .catch((err) => {
       console.log(err);
+    })
+  })
+
+
+router.route('/:id')
+  .get((req, res) => {
+    Card.findAll({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then( card => {
+      res.json(card);
+    })
+
+  })
+  .put((req, res) => {
+    Card.update({
+      title: req.body.title,
+      priority: req.body.priority,
+      status: req.body.status,
+      created_by: req.body.created_by,
+      assigned_to: req.body.assigned_to
+    },{
+      where: {
+        id: req.params.id
+      }
+    })
+    .then( card => {
+        res.json(card);
+    })
+    .catch((err) => {
+      console.log(err.errors);
+      res.json(err.errors[0].message);
     })
   })
 
