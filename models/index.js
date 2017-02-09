0ffc69f126f95d036a8681685e97a69b4b30bd09
Tyler Nichols <tylernichols@Tylers-MacBook-Pro-2.local>
2017-02-08 19:34:13 -1000

    ksldjf

--- models/index.js
+++ models/index.js
@@ -6,10 +6,9 @@ var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var DB_URL    = process.env.DATABASE_URL || null;
-console.log('DB_URL: ', DB_URL);
 var db        = {};
 
-if (process.env.DATABASE_URL) {
+if (DB_URL) {
   var host = 'https://blooming-harbor-32292.herokuapp.com/';
   var sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
